6cd8d72fb728b93beb51fce34d5920684a95fb2c
samstyles84 <sam.styles@arup.com>
2020-09-16 08:53:27 +0100

    added cors and cloudinary

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const apiRouter = require("./routes/api.router");
-// const cors = require("cors");
+const cors = require("cors");
+const cloudinary = require("cloudinary").v2;
+const formData = require("express-form-data");
 
 const {
   handlePSQLErrors,
@@ -9,11 +11,30 @@ const {
   handle500s,
 } = require("./errors");
 
+require("dotenv").config();
+
 const app = express();
 
-// app.use(cors());
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+
+app.use(cors());
+
+//Test whether we need the following or not:
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 app.use(express.json());
+app.use(formData.parse());
 
 app.use("/api", apiRouter);
 
